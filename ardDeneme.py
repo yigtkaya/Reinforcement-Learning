fbbed9aae45f759488b83cefd0c57f56bc2ce508
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-07-11 11:16:48 +0300

    Delete ardDeneme.py

--- ardDeneme.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-
-while True:
-    ser = serial.Serial('/dev/cu.usbserial-10', 9600, timeout=1)
-    value= ser.readline()
-    valueInString=str(value,'UTF-8')
-    print(valueInString)
